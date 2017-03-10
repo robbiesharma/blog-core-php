6dc01ae22b0d1ea2dac20c3c603bd0e1e02a9b51
Robin Sharma <robinsharma2109@gmail.com>
2017-03-10 10:39:30 +0530

    first commit

---
+++ application/views/footer.php
@@ -0,0 +1,9 @@
+    <div id="footer">
+      
+    </div>
+  </div>
+  <script src="<?=  base_url()?>public/js/jquery-2.1.1.min.js"></script>
+  <script src="<?=  base_url()?>public/js/bootstrap.min.js"></script>
+</body>
+</html>
+
